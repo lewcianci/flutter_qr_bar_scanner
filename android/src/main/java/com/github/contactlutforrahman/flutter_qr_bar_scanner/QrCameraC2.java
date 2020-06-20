d454dbdf90963051a3054c31e48fda1e8cfaf772
Lewis Cianci <lewcianci@hotmail.com>
2020-06-20 22:52:34 +1000

    front facing fix

--- android/src/main/java/com/github/contactlutforrahman/flutter_qr_bar_scanner/QrCameraC2.java
+++ android/src/main/java/com/github/contactlutforrahman/flutter_qr_bar_scanner/QrCameraC2.java
@@ -29,7 +29,7 @@ import androidx.annotation.NonNull;
 import static android.hardware.camera2.CameraMetadata.CONTROL_AF_MODE_AUTO;
 import static android.hardware.camera2.CameraMetadata.CONTROL_AF_MODE_CONTINUOUS_PICTURE;
 import static android.hardware.camera2.CameraMetadata.CONTROL_AF_MODE_CONTINUOUS_VIDEO;
-import static android.hardware.camera2.CameraMetadata.LENS_FACING_BACK;
+import static android.hardware.camera2.CameraMetadata.LENS_FACING_FRONT;
 
 /**
  * Implements QrCamera using Camera2 API
@@ -98,7 +98,7 @@ class QrCameraC2 implements QrCamera {
             for (String id : cameraIdList) {
                 CameraCharacteristics cameraCharacteristics = manager.getCameraCharacteristics(id);
                 Integer integer = cameraCharacteristics.get(CameraCharacteristics.LENS_FACING);
-                if (integer != null && integer == LENS_FACING_BACK) {
+                if (integer != null && integer == LENS_FACING_FRONT) {
                     cameraId = id;
                     break;
                 }
